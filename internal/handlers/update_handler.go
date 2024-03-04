33f9cba5a77600fac1c011b96f9087e076164365
Dmitriy Loginov <dmitriy@loginoff.space>
2024-03-05 02:08:56 +0700

    iter1

---
+++ internal/handlers/update_handler.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+)
+
+type MetricsStorage interface {
+	AddMetricValue(mtype string, name string, value string) error
+}
+
+type UpdateHandler struct {
+	storage MetricsStorage
+}
+
+func NewUpdateHandler(storage MetricsStorage) UpdateHandler {
+	return UpdateHandler{
+		storage: storage,
+	}
+}
+
+func (h UpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//Check method type
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	//Change later
+	//Check request format
+	urlParts := strings.Split(r.URL.Path, "/")
+	if len(urlParts) != 5 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	//Change later
+	//Try to add metric to the storage
+	if err := h.storage.AddMetricValue(urlParts[2], urlParts[3], urlParts[4]); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//Write response
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Add("Content-Type", "charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
---
+++ internal/storage/storage.go
@@ -0,0 +1,78 @@
+package storage
+
+import (
+	"errors"
+	"strconv"
+)
+
+const (
+	COUNTER = "counter"
+	GAUGE   = "gauge"
+)
+
+type metric struct {
+	metricType string
+	value      interface{}
+}
+
+type MemStorage struct {
+	metrics map[string]metric
+}
+
+func NewMemStorage() MemStorage {
+	return MemStorage{
+		metrics: make(map[string]metric),
+	}
+}
+
+func (ms *MemStorage) AddMetricValue(mtype string, name string, value string) error {
+	switch mtype {
+	case COUNTER:
+		return ms.addCounterValue(name, value)
+	case GAUGE:
+		return ms.addGaugeValue(name, value)
+	default:
+		return errors.New("wrong metrics type")
+	}
+}
+
+func (ms *MemStorage) addCounterValue(name string, value string) error {
+	//Проверка типа
+	ivalue, err := strconv.ParseInt(value, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	//Проверка существует ли метрика
+	oldValue, ok := ms.metrics[name]
+
+	//Добавление метрики
+	if ok {
+		ms.metrics[name] = metric{
+			metricType: COUNTER,
+			value:      oldValue.value.(int64) + ivalue,
+		}
+	} else {
+		ms.metrics[name] = metric{
+			metricType: COUNTER,
+			value:      ivalue,
+		}
+	}
+
+	return nil
+}
+
+func (ms *MemStorage) addGaugeValue(name string, value string) error {
+	//Проверка типа
+	fvalue, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return err
+	}
+
+	//Добавление/обновление метрики
+	ms.metrics[name] = metric{
+		metricType: GAUGE,
+		value:      fvalue,
+	}
+	return nil
+}
