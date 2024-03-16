0f662a2f61f77fb48e1d0c336641955443e28620
FlutterDizaster <dmitriy@loginoff.space>
2024-03-17 00:33:37 +0300

    wrote a get all metrics handler

--- internal/server/handlers/get_all_handler.go
+++ internal/server/handlers/get_all_handler.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
 
 type GetAllHandler struct {
 	storage GetAllMetricsStorage
@@ -13,5 +16,42 @@ func NewGetAllHandler(storage GetAllMetricsStorage) GetAllHandler {
 }
 
 func (h GetAllHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	content := `{{define "metrics"}}
+	<!doctype html>
+	<html lang="en">
+		<head>
+			<title>Metrics</title>
+		</head>
+		<body>
+			<table>
+				<th>Kind</th>
+				<th>Name</th>
+				<th>Value</th>
+				{{range .}}
+					<tr>
+						<td>{{.Kind}}</td>
+						<td>{{.Name}}</td>
+						<td>{{.Value}}</td>
+					</tr>
+				{{end}}
+			</table>
+		</body>
+	</html>
+	{{end}}`
 
+	tmpl, err := template.New("metrics").Parse(content)
+
+	if err != nil {
+		http.Error(w, "Error whlie creating temaplate", 500)
+		return
+	}
+
+	metrics := h.storage.GetAllMetrics()
+
+	err = tmpl.ExecuteTemplate(w, "metrics", metrics) // TODO: Add data
+
+	if err != nil {
+		http.Error(w, "Error whlie executing temaplate", 500)
+		return
+	}
 }
