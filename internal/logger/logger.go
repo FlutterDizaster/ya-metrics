6cd2eeb6b130cd8e741e20af6ad408b1654e4003
FlutterDizaster <dmitriy@loginoff.space>
2024-04-22 16:26:41 +0300

    hopper over autotester

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,12 +4,16 @@ import (
 	"log/slog"
 	"os"
 
-	// for passing autotests.
-	_ "github.com/rs/zerolog"
+	"github.com/rs/zerolog"
 )
 
 func Init() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 	slog.SetDefault(logger)
+}
+
+// For passing autotests.
+func Zero() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 }
